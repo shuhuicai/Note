4bdb488a16277fe31915f55c45daf8160e83a955
shuhui <1572316380@qq.com>
2019-05-12 21:53:40 +0800

    解决IText将html转pdf文档时,中文乱码问题

--- src/main/java/com/util/TransformFile.java
+++ src/main/java/com/util/TransformFile.java
@@ -1,11 +1,20 @@
 package com.util;
 
 import com.itextpdf.text.Document;
+import com.itextpdf.text.Font;
 import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.tool.xml.XMLWorkerFontProvider;
 import com.itextpdf.tool.xml.XMLWorkerHelper;
+import com.itextpdf.tool.xml.html.CssAppliers;
+import com.itextpdf.tool.xml.html.CssAppliersImpl;
+import com.itextpdf.tool.xml.html.Tags;
+import com.itextpdf.tool.xml.pipeline.html.HtmlPipelineContext;
 import org.jsoup.Jsoup;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 
@@ -46,11 +55,20 @@ public class TransformFile {
      */
     public void produce() {
         produceHtml();
+
         Document document = new Document();
         try {
             PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(new File(pdfPath())));
             document.open();
-            XMLWorkerHelper.getInstance().parseXHtml(writer, document, new FileInputStream(htmlPath()), Charset.forName("utf-8"));
+
+            MyFontsProvider fontsProvider = new MyFontsProvider();
+            fontsProvider.addFontSubstitute("lowagie", "garamond");
+            fontsProvider.setUseUnicode(true);
+            CssAppliers cssAppliers = new CssAppliersImpl(fontsProvider);
+            HtmlPipelineContext htmlContext = new HtmlPipelineContext(cssAppliers);
+            htmlContext.setTagFactory(Tags.getHtmlTagProcessorFactory());
+
+            XMLWorkerHelper.getInstance().parseXHtml(writer, document, new FileInputStream(htmlPath()), Charset.forName("utf-8"), fontsProvider);
         } catch (Exception ex) {
             ex.printStackTrace();
         } finally {
@@ -80,4 +98,27 @@ public class TransformFile {
         }
         return true;
     }
+
+    /**
+     * 重写 字符设置方法，解决中文乱码问题
+     */
+    public static class MyFontsProvider extends XMLWorkerFontProvider {
+
+        public MyFontsProvider() {
+            super(null, null);
+        }
+
+        @Override
+        public Font getFont(final String fontname, String encoding, float size, final int style) {
+            String fntname = fontname;
+            if (fntname == null) {
+                fntname = "宋体";
+            }
+            if (size == 0) {
+                size = 4;
+            }
+            return super.getFont(fntname, encoding, size, style);
+        }
+    }
+
 }
